11cd89007ddf9166c41fbd7db52d9553166cb099
Thiago Marinho <tgmarinho@gmail.com>
2019-11-06 11:11:31 -0400

    Server Side Rendering com NextJS - aula 3 - configurando o Head passando o title de cada página

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import React from "react";
-
 import Link from "next/link";
+import Head from "next/head";
 
 export default function home() {
   return (
     <div>
+      <Head>
+        <title>Home</title>
+      </Head>
       <h1>Oi</h1>
 
       <Link href="/users">
--- pages/users.js
+++ pages/users.js
@@ -1,11 +1,14 @@
 import React, { useEffect } from "react";
 import axios from "axios";
-
 import Link from "next/link";
+import Head from "next/head";
 
 export default function User({ users }) {
   return (
     <div>
+      <Head>
+        <title>Usuários</title>
+      </Head>
       <ul>
         {users.map(user => (
           <li key={user.id}>{user.login}</li>
